43f5bff1713b582e96b7ae54f09da3738e797744
Jeffrey Gates <jeffreymrgates@gmail.com>
2026-03-09 15:17:58 -0400

    Added lines 1-45, created 3 methods using method overloading

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -2,9 +2,43 @@
 {
     public class Program
     {
+        public static int Add(int numOne, int numTwo)
+        {
+            return numOne + numTwo;
+        }
+
+        public static decimal Add(decimal numOne, decimal numTwo)
+        {
+            return numOne + numTwo;
+        }
+
+        public static string Add(int numOne, int numTwo, bool isCurrency)
+        {
+            var sum = numOne + numTwo;
+            if (isCurrency && sum > 1)
+            {
+                return $"{sum} dollars";
+            }
+            else if (isCurrency && sum == 1)
+            {
+                return $"{sum} dollar";
+            }
+            else
+            {
+                return sum.ToString();
+            }
+        }
+
+        public static double Add(double numOne, double numTwo)
+        {
+            return numOne + numTwo;
+        }
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(Add(2, 2));
+            Console.WriteLine(Add(2, 2.5m));
+            Console.WriteLine(Add(1, 1, true));
         }
     }
 }
